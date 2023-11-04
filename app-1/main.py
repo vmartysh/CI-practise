a8c308c6439ddb5862a46fe046b55a045094c636
vmartysh <79521917+vmartysh@users.noreply.github.com>
2023-11-04 17:56:46 +0200

    Added dirs

---
+++ app-1/main.py
@@ -0,0 +1,16 @@
+from google.cloud import secretmanager_v1
+
+def sample_access_secret_version():
+    # Create a client
+    client = secretmanager_v1.SecretManagerServiceClient()
+
+    # Initialize request argument(s)
+    request = secretmanager_v1.AccessSecretVersionRequest(
+        name="projects/35212339715/secrets/sftp-secret/versions/latest",
+    )
+
+    # Make the request
+    response = client.access_secret_version(request=request)
+
+    # Handle the response
+    print(response)
